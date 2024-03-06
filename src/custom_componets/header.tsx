4d314c00b2a1cfa617a71ce4de2eb7aa12e40061
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2024-03-06 20:57:28 +0500

    FEAT: Completed mobile responsive drawer

--- src/custom_componets/header.tsx
+++ src/custom_componets/header.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { VscThreeBars } from "react-icons/vsc";
+import {
+  VscThreeBars,
+  VscLock,
+} from "react-icons/vsc";
 import {
   Sheet,
   SheetClose,
@@ -30,23 +33,35 @@ export default function Header() {
       </div>
       <div className="hidden xl:flex xl:visible xl:justify-between xl:items-center xl:gap-10">
         <div className="flex justify-between items-center xl:gap-4">
-          <Link href={"#"} className="transition ease-in-out scale-90 hover:scale-100">
-            <p className="text-black hover:text-[#f0a535]">
+          <Link
+            href={"#"}
+            className="transition ease-in-out scale-90 hover:scale-100"
+          >
+            <p className="text-black hover:text-[#f0a535] xl:text-lg font-bold">
               Home
             </p>
           </Link>
-          <Link href={"#"} className="transition ease-in-out scale-90 hover:scale-100">
-            <p className="text-black hover:text-[#f0a535]">
+          <Link
+            href={"#"}
+            className="transition ease-in-out scale-90 hover:scale-100"
+          >
+            <p className="text-black hover:text-[#f0a535] xl:text-lg font-bold">
               Feature
             </p>
           </Link>
-          <Link href={"#"} className="transition ease-in-out scale-90 hover:scale-100">
-            <p className="text-black hover:text-[#f0a535]">
+          <Link
+            href={"#"}
+            className="transition ease-in-out scale-90 hover:scale-100"
+          >
+            <p className="text-black hover:text-[#f0a535] xl:text-lg font-bold">
               Pricing
             </p>
           </Link>
-          <Link href={"#"} className="transition ease-in-out scale-90 hover:scale-100">
-            <p className="text-black hover:text-[#f0a535]">
+          <Link
+            href={"#"}
+            className="transition ease-in-out scale-90 hover:scale-100"
+          >
+            <p className="text-black hover:text-[#f0a535] xl:text-lg font-bold">
               Login
             </p>
           </Link>
@@ -71,20 +86,59 @@ export default function Header() {
             <div>
               <div
                 id="nav-options"
-                className="flex flex-col justify-center items-start gap-3"
+                className="flex flex-col justify-center items-start gap-8 py-8"
               >
-                <Link href="#">
-                  <h1>Home</h1>
-                </Link>
-                <Link href="#">
-                  <h1>Feature</h1>
-                </Link>
-                <Link href="#">
-                  <h1>Pricing</h1>
-                </Link>
-                <Link href="#">
-                  <h1>Login</h1>
-                </Link>
+                <div className="relative group cursor-pointer w-full">
+                  <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+                  <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                    <div className="space-y-2">
+                      <Link href="#">
+                        <h1 className="text-lg font-extrabold">
+                          Home
+                        </h1>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="relative group cursor-pointer w-full">
+                  <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+                  <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                    <div className="space-y-2">
+                      <Link href="#">
+                        <h1 className="text-lg font-extrabold">
+                          Feature
+                        </h1>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="relative group cursor-pointer w-full">
+                  <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+                  <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                    <div className="space-y-2">
+                      <Link href="#">
+                        <h1 className="text-lg font-extrabold">
+                          Pricing
+                        </h1>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="relative group cursor-pointer w-full">
+                  <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+                  <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                    <div className="space-y-2">
+                      <Link href="#">
+                        <h1 className="text-lg font-extrabold">
+                          Login{" "}
+                        </h1>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </SheetContent>
